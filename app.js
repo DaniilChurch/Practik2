0adf0f511c024e475efcfe60fae1ce3251d93d10
Daniil <colbaska.chiter@mail.com>
2022-07-01 21:11:13 +0300

    Основы

---
+++ app.js
@@ -0,0 +1,39 @@
+// 1 Переменные
+const name = 'Даниил'
+//const lastName = 'Горобцов'
+// let age = 15
+const isProgrammer = true
+
+const _private = 'private'
+const $ = '$'
+
+const widthNum5 = 5
+// const 5widthNum5 = 5 //error
+
+// 2 Мутирование
+// console.log('Имя человека: ' + name + ', возраст: ' + age)
+//alert('Имя человека: ' + name + ', возраст: ' + age)
+
+// const lastName = prompt('Введите фамилию:')
+// alert(name + ' ' + lastName)
+
+// 3 Операторы
+let currentYear = 2022
+const birtYear = 2007
+//const age = currentYear - birtYear
+
+const a = 10
+const b = 5
+
+let c = 32
+//c = c + a
+c += 2
+
+console.log(a / b)
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+
+console.log(++currentYear)
+console.log(c)
+
