9a55fc98df330fc5c2ef25ceda5f0582e60e44be
Daniil <colbaska.chiter@mail.com>
2022-07-02 21:25:50 +0300

    Основы

--- app.js
+++ app.js
@@ -1,39 +1,102 @@
-// 1 Переменные
-const name = 'Даниил'
-//const lastName = 'Горобцов'
-// let age = 15
-const isProgrammer = true
-
-const _private = 'private'
-const $ = '$'
-
-const widthNum5 = 5
-// const 5widthNum5 = 5 //error
-
-// 2 Мутирование
-// console.log('Имя человека: ' + name + ', возраст: ' + age)
-//alert('Имя человека: ' + name + ', возраст: ' + age)
-
-// const lastName = prompt('Введите фамилию:')
-// alert(name + ' ' + lastName)
-
-// 3 Операторы
-let currentYear = 2022
-const birtYear = 2007
-//const age = currentYear - birtYear
-
-const a = 10
-const b = 5
-
-let c = 32
+// // 1 Переменные
+// const name = 'Даниил'
+// //const lastName = 'Горобцов'
+// // let age = 15
+// const isProgrammer = true
+//
+// const _private = 'private'
+// const $ = '$'
+//
+// const widthNum5 = 5
+// // const 5widthNum5 = 5 //error
+//
+// // 2 Мутирование
+// // console.log('Имя человека: ' + name + ', возраст: ' + age)
+// //alert('Имя человека: ' + name + ', возраст: ' + age)
+//
+// // const lastName = prompt('Введите фамилию:')
+// // alert(name + ' ' + lastName)
+//
+// // 3 Операторы
+// let currentYear = 2022
+// const birtYear = 2007
+// //const age = currentYear - birtYear
+//
+// const a = 10
+// const b = 5
+//
+// let c = 32
 //c = c + a
-c += 2
+// c += 2
+// c -= 2
+// c *= 2
+// c /= 2
+//
+// console.log(a / b)
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+//
+// console.log(++currentYear)
+// console.log(c)
 
-console.log(a / b)
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
+// 4 Типы данных
+// const isPogrammer = true
+// const name = 'Даниил'
+// const age = 15
+// console.log(typeof isPogrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+// console.log(typeof null)
 
-console.log(++currentYear)
-console.log(c)
+// 5 Приоритеты операторов
+// const  fullAge = 15
+// const birthYear = 2007
+// const currentYear = 2022
+//
+// const isFullAge = currentYear - birthYear >= fullAge
+// console.log(isFullAge)
+
+// 6 Условные операторы
+// const cookingStatus = 'fail' // ready, fail, pending
+//
+// if (cookingStatus === 'ready') {
+//     console.log('Печеньки готовы и их можно кушать')
+// } else if (cookingStatus === 'pending') {
+//         console.log('Печеньки находятся в процессе приготовления')
+//     } else {
+//     console.log('Где печеньки?:(')
+// }
+
+const isReady = true
+//
+// if (isReady === true) {
+//     console.log('Все готово!')
+// } else {
+//     console.log('Все не готово!')
+// }
+
+// Тенарное выражение
+isReady ? console.log('Все готово!') : console.log('Все не готово!')
+
+// const num1 = 42
+// const num2 = '42'
+//
+// console.log(num1 === num2)
+
+// 7 Булевая логика
+
+// 8 Функции
+
+
+
+
+
+
+
+
+
+
+
 
